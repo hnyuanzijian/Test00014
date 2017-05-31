51e1c2d0d12c5c23b6e4ace571e2618008e8e099
hnyuanzijian <18738395879@163.com>
2017-05-31 15:30:00 +0800

    collection

---
+++ src/com/yuanzijian02/itcast/collection/Demo1.java
@@ -0,0 +1,16 @@
+package com.yuanzijian02.itcast.collection;
+/*
+ * 数组：存储同一数据类型的集合容器
+ * 
+ * 数组的特点：
+ * 
+ * */
+public class Demo1 {
+	public static void main(String[] args) {
+		Object[] arr = new Object[10];
+		arr[1] = "abc";
+		arr[2] = 'a';
+		arr[3]  = 12;
+		
+	}
+}
